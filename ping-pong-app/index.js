3554176ad2a1154b69b268041f152261682b9dda
AhmedHodhod <alhdhody@gmail.com>
2023-06-20 14:45:50 +0300

    changed port to 80

--- ping-pong-app/index.js
+++ ping-pong-app/index.js
@@ -5,7 +5,7 @@
   const express = require("express");
   const app = express();
 
-  const port = process.env.PORT || 5001;
+  const port = process.env.PORT || 80;
 
   const axios = require('axios');
 
